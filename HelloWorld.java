611bcf6c90b91fabc53f2f3716f4c66ed7613ddf
rintansyelaa <rintan_syela_@student.ub.ac.id>
2023-11-06 19:51:21 +0700

    update

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
-        System.out.println();
+        System.out.println("Halo");
     }
 }
